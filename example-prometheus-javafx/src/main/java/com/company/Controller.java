27e25865e80b05251ce15ad76a76a415eec528f4
Luca Molteni <volothamp@gmail.com>
2019-04-19 11:15:38 +0200

    New JavaFX Client

---
+++ example-prometheus-javafx/src/main/java/com/company/Controller.java
@@ -0,0 +1,15 @@
+package com.company;
+
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.Label;
+
+public class Controller {
+
+    public Label helloWorld;
+
+    public void sayHelloWorld(ActionEvent actionEvent) {
+        helloWorld.setText("Hello world!");
+    }
+
+}
